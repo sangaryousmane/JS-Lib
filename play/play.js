a3a0763bc18acc2e7c0df9da4db4ef38f865c47f
ousmane <sangaryousmane@gmail.com>
2022-07-31 23:55:16 +0800

    New Updated branch

---
+++ play/play.js
@@ -0,0 +1,17 @@
+
+const btnGreen=document.querySelector("btn");
+const bgColor=document.querySelector("bg");
+
+
+btnGreen.addEventListener('click', function(){
+    if(btnGreen){
+        bgColor.style.backgroundColor='green';
+    }
+});
+
+
+
+
+
+
+
