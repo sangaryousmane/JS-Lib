19fe12f714f422eb366ada19a1e5c0e3ef9bd59f
ousmane <sangaryousmane@gmail.com>
2022-08-01 10:34:35 +0800

    A method that simulates pets actions

---
+++ jump/jump.js
@@ -0,0 +1,10 @@
+
+const frog=()=> console.log('Thug Thug!!');
+const dog=()=> console.log('Bark Bark!!');
+const names=(dogName, frogName)=>{
+    console.log(`The dog name is ${dogName} and the frog name is ${frogName}`);
+};
+
+dog();
+frog();
+names('Tuppy', 'Froggy');
